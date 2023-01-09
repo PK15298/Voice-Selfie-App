b960a693e875d8e0b7eb15b7c9d0c4e58abb4567
Navya-Seth <91605093+Navya-Seth@users.noreply.github.com>
2023-01-09 20:22:24 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,12 +1,32 @@
 var SpeechRecognition = window.webkitSpeechRecognition;
+
 var recognition = new SpeechRecognition();
-var Textbox = document.getElementById("box");
+
 
 function start(){
-    Textbox.innerHTML = "" ;
+    document.getElementById("box").innerHTML="";
     recognition.start();
 }
 
 recognition.onresult = function(event){
     console.log(event);
+    var content = event.results[0][0].transcript;
+    document.getElementById("box").innerHTML=content;
+    console.log(content);
+    speak();
 }
+
+function speak(){
+    var syn = window.SpeechSynthesis;
+    speak_data = document.getElementById("box").innerHTML;
+    var utterThis = new SpeechSynthesisUtterance(speak_data);
+    syn.speak(utterThis);
+    Webcam.attach(camera);
+}
+camera = document.getElementById("cam");
+Webcam.set({
+    width:360,
+    hright:250,
+    image_format:'png',
+    png_quality:90    
+});
